fd002f8f5e30d593550f8eb46abeca917830ddf2
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-26 00:39:18 +0100

    add oauth1 method and access token

--- api/Api.py
+++ api/Api.py
@@ -2,7 +2,7 @@ import requests
 from requests_oauthlib import OAuth1
 
 # change with configUser
-import config as ApiInfo
+import config as api_info
 
 
 class Api:
@@ -10,9 +10,12 @@ class Api:
         """
          used to connect to twitter api
         """
-        self.__api_key: str = ApiInfo.API_KEY
-        self.__api_secret_key: str = ApiInfo.API_SECRET_KEY
-        self.__bearer_token: str = ApiInfo.BEARER_TOKEN
+        self.__API_KEY: str = api_info.API_KEY
+        self.__API_SECRET_KEY: str = api_info.API_SECRET_KEY
+        self.__BEARER_TOKEN: str = api_info.BEARER_TOKEN
+        self.__ACCESS_TOKEN: str = api_info.ACCESS_TOKEN
+        self.__ACCESS_TOKEN_SECRET: str = api_info.ACCESS_TOKEN_SECRET
 
     def __auth_oauth1(self):
-        return OAuth1(client_key=self.__api_key, client_secret=self.__api_secret_key, resource_owner_key=)
+        return OAuth1(client_key=self.__API_KEY, client_secret=self.__API_SECRET_KEY,
+                      resource_owner_key=self.__ACCESS_TOKEN, resource_owner_secret=self.__ACCESS_TOKEN_SECRET)
