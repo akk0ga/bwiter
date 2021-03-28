261c5eeba84b371bb057115db0611376d6611631
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-28 11:51:23 +0200

    add init and display method

---
+++ notification/Notify.py
@@ -0,0 +1,9 @@
+class Notify:
+    def __init__(self, title: str, desc: str, like_link: str, show_link: str):
+        self.__title: str = title
+        self.__desc: str = desc
+        self.__like_link: str = like_link
+        self.__show_link: str = show_link
+
+    def display(self):
+        pass
