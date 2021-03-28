0a4993707fac6dfd161541bba6e8102734ae803b
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-28 15:05:39 +0200

    update App

--- App.py
+++ App.py
@@ -4,34 +4,29 @@ import datetime
 
 
 class App:
-
-    def last_search(self, t0):
-        calc: bool = True
-        if calc:
-            t1 = datetime.datetime.now()
+    def __init__(self):
+        self.__user = User()
 
     def run(self):
-        user = User()
+        req_complete: bool = False
         print('bot is running...')
         time.sleep(0.5)
-        req_done: int = 0
         while True:
+            # launch request (2 per sec) to get the last tweet and check if the tweet is already show
             time_req: datetime = datetime.datetime.now()
             microsecond: int = time_req.microsecond
-            second: int = time_req.second
-
-            if 240_000 <= microsecond <= 260_000 and req_done == 0:
-                print(f'last tweet {user.last_tweet(screen_name="_dieuoff")}')
+            if 200_000 <= microsecond <= 300_000 and not req_complete:
                 print(microsecond)
-                req_done = 1
-            if 740_000 <= microsecond <= 760_000 and req_done == 1:
-                print(f'last tweet {user.last_tweet(screen_name="_dieuoff")}')
-                print(microsecond)
-                req_done = 0
+                req_complete = True
+                print(self.__user.last_tweet(screen_name="_dieuoff"))
 
-        # print(user.get_users_list())
-        # print(f'single user {user.get_user(user_id="2295631308", screen_name="RBW_MAMAMOO")}')
-        # print(f'last tweet {user.last_tweet(screen_name="_dieuoff")}')
+            if 700_000 <= microsecond <= 800_000 and req_complete:
+                print(microsecond)
+                req_complete = False
+                print(self.__user.last_tweet(screen_name="_dieuoff"))
+
+    # print(user.get_users_list())
+    # print(f'single user {user.get_user(user_id="2295631308", screen_name="RBW_MAMAMOO")}')
 
 
 if __name__ == '__main__':
