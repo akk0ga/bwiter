7af53e74fc54acd4aaea0d900e48eedb3069ad42
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-30 00:35:56 +0200

    create Bot class

--- App.py
+++ App.py
@@ -1,26 +1,12 @@
-from twitter.User import User
-from api.Api import Api
-from notification.Notification import Notification
-from threading import Timer
+from bot.Bot import Bot
 
 
 class App:
     def __init__(self):
-        self.__user = User(screen_name='_dieuoff')
-        self.__api = Api()
-        self.__notify = Notification()
-
-    def __search_new_tweet(self, refresh_time: int = 30):
-        if refresh_time >= 1:
-            print('search...')
-            print(self.__user.last_tweet())
-            Timer(refresh_time, self.__search_new_tweet).start()
-        else:
-            raise Exception('you can\'t have less than 30 second (api rate limit u know)')
+        self.__bot = Bot()
 
     def run(self):
-        self.__notify.bot_status('run')
-        self.__search_new_tweet()
+        self.__bot.search_tweet()
 
 
 if __name__ == '__main__':
---
+++ bot/Bot.py
@@ -0,0 +1,24 @@
+from twitter.User import User
+from notification.Notification import Notification
+from threading import Timer
+
+
+class Bot:
+    def __init__(self, refresh_time: int = 30):
+        self.__notify: Notification = Notification()
+        self.__user: User = User(screen_name='_dieuoff')
+        self.__refresh_time: int = refresh_time if refresh_time >= 30 else 30
+        self.__run: bool = False
+
+    def search_tweet(self):
+        """
+        search new tweet each x second
+        :return:
+        """
+        self.__notify.bot_status('run')
+        print('search...')
+        print(self.__user.last_tweet())
+        Timer(self.__refresh_time, self.search_tweet).start()
+    
+    def display_status(self):
+        self.__notify.bot_status('run' if not self.__run else 'stop')
